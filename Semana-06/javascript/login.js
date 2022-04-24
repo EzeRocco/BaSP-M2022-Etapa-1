206f2a505df8e3f73274dca821a88b639fbcbce3
EzeRocco <eserocco97@gmail.com>
2022-04-24 15:25:41 -0300

    BaSP-W06-2022: modification validation of email in login file

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -4,7 +4,10 @@ window.onload = function() {
     var password = document.getElementById("password");
     var error_password = document.getElementById("error-password");
     var form = document.getElementById("login-form");
-    var bottom = document.getElementById("bottom")
+    var bottom = document.getElementById("bottom");
+    var flag;
+    var final_validation;
+    var final_validation2;
 
     error_email.style.display = "none";
     error_password.style.display = "none";
@@ -14,19 +17,46 @@ window.onload = function() {
     })
 
     email.addEventListener('blur',function(){
-        var comprobation = email.value.indexOf("@");
-        var comprobation2 = email.value.indexOf(".com");  
-        if (comprobation=== -1 || comprobation2 === -1){
+        var validation = email.value.indexOf("@");
+        var validation2 = email.value.indexOf(".com");  
+        if (validation === -1 || validation2 === -1){
             error_email.style.display = "flex";
             error_email.style.color = "red";
-            email.style.borderColor= "red";
+            email.style.borderColor = "red";
         } else {
             error_email.style.display = "none";
-            email.style.borderColor= "green";
-            var final_comprobation=2;
+            email.style.borderColor = "green";
+            final_validation = 1;
         }
     })
 
+    password.addEventListener('focus', function(){
+        error_password.style.display = "none";
+    })
+
+    password.addEventListener('blur', function(){
+        flag = 0;
+        if (password.value.length < 7 || password.value.length === 0) {
+            flag = -1;
+        }
+        if (flag===-1){
+            error_password.style.display = "flex";
+            error_password.style.color = "red";
+            password.style.borderColor= "red";
+        } else {
+            error_password.style.display = "none";
+            password.style.borderColor = "green";
+            final_validation2 = 1;
+        }
+    })
+
+    bottom.addEventListener('click', function(){
+        if((final_validation === 1) && (final_validation2 === 1)){
+            window.alert("You logged in successfully");
+        } else {
+            window.alert("Email or password was not correct");
+        }
+    })
 }
 
 
